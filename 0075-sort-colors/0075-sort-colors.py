dbceda8ab62b9b5fbeb86e34342200cb57e6a81e
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-06-05 21:23:15 +0530

    Time: 53 ms (24.25%), Space: 16.4 MB (15.11%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -5,22 +5,36 @@ class Solution:
         """
         # nums[:] = sorted(nums)
         
-        zero_cnt , one_cnt , two_cnt = 0 ,0,0
+#         zero_cnt , one_cnt , two_cnt = 0 ,0,0
         
-        for num in nums :
-            if num == 0 :
-                zero_cnt += 1
-            elif num == 1 :
-                one_cnt += 1
-            else : two_cnt += 1
+#         for num in nums :
+#             if num == 0 :
+#                 zero_cnt += 1
+#             elif num == 1 :
+#                 one_cnt += 1
+#             else : two_cnt += 1
         
-        for i in range(0,len(nums)) :
-            if zero_cnt > 0 :
-                nums[i] = 0
-                zero_cnt -= 1
-            elif one_cnt > 0 :
-                nums[i] = 1
-                one_cnt -= 1
-            elif two_cnt > 0 :
-                nums[i] = 2
-                two_cnt -= 1
+#         for i in range(0,len(nums)) :
+#             if zero_cnt > 0 :
+#                 nums[i] = 0
+#                 zero_cnt -= 1
+#             elif one_cnt > 0 :
+#                 nums[i] = 1
+#                 one_cnt -= 1
+#             elif two_cnt > 0 :
+#                 nums[i] = 2
+#                 two_cnt -= 1
+
+        low , mid , high = 0 , 0, len(nums) - 1
+         
+        while mid <= high :
+                if nums[mid] == 0 :
+                    nums[low] , nums[mid] = nums[mid] , nums[low]
+                    low += 1 
+                    mid += 1
+                elif nums[mid] == 1 :
+                    mid += 1
+                elif nums[mid] == 2 :
+                    nums[mid] , nums[high] = nums[high] , nums[mid]
+                    high -= 1
+            

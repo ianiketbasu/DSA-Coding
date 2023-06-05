9c63187bec74fed95a87bddfae42ef315133a8b4
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-06-05 22:12:05 +0530

    Time: 56 ms (20.08%), Space: 16.4 MB (8.38%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -5,9 +5,12 @@ class Solution:
         """
         n = len(matrix)
         
-        for i in range(0,n) :
-            for j in range(0,i) :
-                matrix[i][j] , matrix[j][i] = matrix[j][i] , matrix[i][j]
+#         for i in range(0,n) :
+#             for j in range(0,i) :
+#                 matrix[i][j] , matrix[j][i] = matrix[j][i] , matrix[i][j]
         
-        for i in range(0,n) :
-            matrix[i] = reversed(matrix[i])
+#         for i in range(0,n) :
+#             matrix[i] = reversed(matrix[i])
+
+        matrix[:] = zip(*matrix[::-1])
+    

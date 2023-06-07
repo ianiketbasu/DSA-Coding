ae79bdf0a16916568b3b41b4fbd890050d2abff6
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-06-07 12:00:13 +0530

    Time: 188 ms (14.48%), Space: 18 MB (21.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,18 +1,26 @@
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         my_map = {}
+        
+#         for num in nums :
+#             if num in my_map :
+#                 my_map[num] += 1
+#             else :
+#                 my_map[num] = 1
+#         ans = None
+#         count = float('-inf')
+        
+#         for key,value in my_map.items() :
+#             if value > count :
+#                 ans = key
+#                 count = value
+        
+#         return ans
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         my_map = {}
-        
         for num in nums :
-            if num in my_map :
-                my_map[num] += 1
-            else :
-                my_map[num] = 1
-        ans = None
-        count = float('-inf')
-        
-        for key,value in my_map.items() :
-            if value > count :
-                ans = key
-                count = value
-        
-        return ans
+            my_map[num] = my_map.get(num , 0) + 1
+            if my_map[num] > len(nums)/2 :
+                return num

20e899d890cb12bce273df92d4b1c8422ed55cbd
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-06-08 11:59:37 +0530

    Time: 124 ms (64.09%), Space: 17.8 MB (17.88%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -15,33 +15,39 @@ class Solution:
 #         return li
 
 
-        counter1 , counter2 , ele1 , ele2 = 0 ,0 , None , None
+#         counter1 , counter2 , ele1 , ele2 = 0 ,0 , None , None
         
-        for num in nums :
-            if counter1 == 0 and num != ele2 :
-                ele1 = num
-                counter1 += 1
-            elif counter2 == 0 and num != ele1 :
-                ele2 = num
-                counter2 += 1
-            elif num == ele1 :
-                counter1 += 1
-            elif num == ele2 :
-                counter2 += 1
-            else :
-                counter1 -= 1
-                counter2 -= 1
+#         for num in nums :
+#             if counter1 == 0 and num != ele2 :
+#                 ele1 = num
+#                 counter1 += 1
+#             elif counter2 == 0 and num != ele1 :
+#                 ele2 = num
+#                 counter2 += 1
+#             elif num == ele1 :
+#                 counter1 += 1
+#             elif num == ele2 :
+#                 counter2 += 1
+#             else :
+#                 counter1 -= 1
+#                 counter2 -= 1
         
-        counter1 , counter2 = 0 , 0
-        for num in nums :
-            if ele1 == num :
-                counter1 += 1
-            elif ele2 == num :
-                counter2 += 1
-        li = []  
-        if counter1 > len(nums) / 3 :
-            li.append(ele1)
-        if counter2 > len(nums) / 3 :
-            li.append(ele2)
+#         counter1 , counter2 = 0 , 0
+#         for num in nums :
+#             if ele1 == num :
+#                 counter1 += 1
+#             elif ele2 == num :
+#                 counter2 += 1
+#         li = []  
+#         if counter1 > len(nums) / 3 :
+#             li.append(ele1)
+#         if counter2 > len(nums) / 3 :
+#             li.append(ele2)
         
-        return li
+#         return li
+
+        return [n for n in set(nums) if nums.count(n) > len(nums) / 3]
+
+
+
+

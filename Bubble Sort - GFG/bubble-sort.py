f9bb3275e35ae6e94dc6c6a53246b32cd02e8d7f
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-06-10 11:22:47 +0530

    Added solution - LeetHub

--- Bubble Sort - GFG/bubble-sort.py
+++ Bubble Sort - GFG/bubble-sort.py
@@ -3,10 +3,15 @@
 class Solution:
     #Function to sort the array using bubble sort algorithm.
     def bubbleSort(self,arr, n):
+        isSwap = False
         for i in range(n-1,0,-1):
             for j in range(0,i):
                 if arr[j] > arr[j+1] :
                     arr[j] , arr[j+1] = arr[j+1] , arr[j]
+                    isSwap = True
+            
+            if isSwap == False :
+                break
         
         
 

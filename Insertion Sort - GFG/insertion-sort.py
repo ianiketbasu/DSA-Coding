07886e0cfea7fb031a043432ac39b398f909f445
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-06-10 11:34:22 +0530

    Added solution - LeetHub

---
+++ Insertion Sort - GFG/insertion-sort.py
@@ -0,0 +1,30 @@
+#Sort the array using insertion sort
+
+class Solution:
+    def insert(self, alist, j, n):
+        #code here
+        while(j > 0 and alist[j-1] > alist[j]) :
+            alist[j-1] , alist[j] = alist[j] , alist[j-1]
+            j -= 1
+    #Function to sort the list using insertion sort algorithm.    
+    def insertionSort(self, alist, n):
+        for i in range(0,n) :
+            j = i
+            self.insert(alist,j,n)
+
+
+#{ 
+ # Driver Code Starts
+if __name__=="__main__":
+    t=int(input())
+    for i in range(t):
+        n=int(input())
+        arr=list(map(int,input().split()))
+    
+        Solution().insertionSort(arr,n)
+    
+        for i in range(n):
+            print(arr[i],end=" ")
+    
+        print()
+# } Driver Code Ends

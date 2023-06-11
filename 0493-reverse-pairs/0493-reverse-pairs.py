da3a57fca044f0ad321969b6184c60826bbf5d6d
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-06-11 12:30:31 +0530

    Time: 1738 ms (44.12%), Space: 24.2 MB (8.82%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.py
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -4,17 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        
+        #For merging the two array based on index
         def merge(nums, low, mid, high):
             left = low
             right = mid + 1
             li = []
-            count = 0
-            
-            ptr = mid + 1
-            for i in range(low,mid+1) :
-                while ptr <= high and nums[i] > 2*nums[ptr] : ptr += 1
-                count += (ptr - (mid + 1))
-            
 
             while left <= mid and right <= high:
                 if nums[left] <= nums[right]:
@@ -34,9 +29,17 @@ class Solution(object):
 
             nums[low:high + 1] = li
 
-            return count
-
             
+        
+        # Counting the Valid pair 
+        def countThePair(num,low,mid,high) :
+            count = 0
+            
+            ptr = mid + 1
+            for i in range(low,mid+1) :
+                while ptr <= high and nums[i] > 2*nums[ptr] : ptr += 1
+                count += (ptr - (mid + 1))
+            return count
             
             
         def getPairCount(nums,low,high) :
@@ -45,8 +48,8 @@ class Solution(object):
                 mid = (low + high) // 2
                 count += getPairCount(nums,low,mid)
                 count += getPairCount(nums,mid+1,high)
-                
-                count += merge(nums,low,mid,high)
+                count += countThePair(nums,low,mid,high)
+                merge(nums,low,mid,high)
             
             return count
         

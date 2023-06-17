6938ccec3b41ad5d148b7d175646475ee492917c
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-06-17 15:49:46 +0530

    Time: 143 ms (44.14%), Space: 18.9 MB (22.48%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,24 +1,41 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n = len(height)
+#         n = len(height)
 
-        if n < 3: 
-            return 0
+#         if n < 3: 
+#             return 0
 
-        leftMax = [0] * n
-        rightMax = [0] * n
+#         leftMax = [0] * n
+#         rightMax = [0] * n
 
-        lMax, rMax = height[0], height[n - 1]
+#         lMax, rMax = height[0], height[n - 1]
 
-        for i in range(1, n - 1):
-            leftMax[i] = lMax
-            lMax = max(lMax, height[i])
+#         for i in range(1, n - 1):
+#             leftMax[i] = lMax
+#             lMax = max(lMax, height[i])
 
-            rightMax[n - i - 1] = rMax
-            rMax = max(rMax, height[n - i - 1])
-        water = 0
-        for i in range(1,n-1) :
-            if height[i] < leftMax[i] and height[i] < rightMax[i] :
-                water += min(leftMax[i],rightMax[i]) - height[i]
+#             rightMax[n - i - 1] = rMax
+#             rMax = max(rMax, height[n - i - 1])
+#         water = 0
+#         for i in range(1,n-1) :
+#             if height[i] < leftMax[i] and height[i] < rightMax[i] :
+#                 water += min(leftMax[i],rightMax[i]) - height[i]
+        
+#         return water
+
+
+        water , level = 0 , 0
+        l , r = 0 , len(height) - 1
+        
+        while l < r :
+            lower = height[l] if height[l] < height[r] else height[r]
+            level = max(lower,level)
+            
+            water += level - lower
+            
+            if height[l] < height[r] :
+                l += 1
+            else :
+                r -= 1
         
         return water

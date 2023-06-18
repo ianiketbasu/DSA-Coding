911f63f96c112a0809520673b67930a0d8512fc4
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-06-18 10:49:10 +0530

    Time: 2812 ms (20.88%), Space: 35.8 MB (75.73%) - LeetHub

---
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countPaths(self, A: List[List[int]]) -> int:
+        m, n, mod = len(A), len(A[0]), 10 ** 9 + 7
+        dp = [[1] * n for i in range(m)]
+        for a, i, j in sorted([A[i][j], i, j] for i in range(m) for j in range(n)):
+            for x, y in [[i, j + 1], [i, j - 1], [i + 1, j], [i - 1, j]]:
+                if 0 <= x < m and 0 <= y < n and A[x][y] < A[i][j]:
+                    dp[i][j] += dp[x][y] % mod
+        return sum(map(sum, dp)) % mod

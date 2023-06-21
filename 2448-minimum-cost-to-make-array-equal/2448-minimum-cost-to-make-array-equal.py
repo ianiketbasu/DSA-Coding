7ae420c29c12b1d5ca969165fe3ffdbd0836ffbd
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-06-21 12:43:39 +0530

    Time: 937 ms (36.76%), Space: 31.8 MB (91.18%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
@@ -1,10 +1,12 @@
 class Solution:
     def minCost(self, nums: List[int], cost: List[int]) -> int:
-        def calCost(targetVal) :
-            totalCost = 0
-            for i in range(0,len(nums)):
-                totalCost += abs(targetVal-nums[i])*cost[i]
-            return totalCost
+        def calCost(a) :
+            # totalCost = 0
+            # for i in range(0,len(nums)):
+            #     totalCost += abs(targetVal-nums[i])*cost[i]
+            # return totalCost
+            
+            return sum(abs(a-b)*c for b,c in zip(nums,cost))
         
         low , high= min(nums) , max(nums)
         mid = (low + high) // 2

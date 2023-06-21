c1924dcc4bbe97cf35d09d82de31be8c8265f26f
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-06-21 20:21:24 +0530

    Time: 11 ms (70.76%), Space: 8.1 MB (43.84%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char,int> mpp;
+        mpp['I'] = 1;
+        mpp['V'] = 5;
+        mpp['X'] = 10;
+        mpp['L'] = 50;
+        mpp['C'] = 100;
+        mpp['D'] = 500;
+        mpp['M'] = 1000;
+        
+//         int ans = 0 , i = 0 , num;
+        
+//         while(i < s.size()){
+//             if(i == s.size() - 1 or mpp[s[i]] >= mpp[s[i + 1]]) 
+//                 num = mpp[s[i++]];
+            
+//             else{
+//                 num = mpp[s[i+1]] - mpp[s[i]];
+//                 i += 2;
+//             }
+//             ans += num;
+//         }
+        
+//         return ans;
+        
+        int val = 0;
+        for(int i=0;i<s.size()-1;i++){
+            if(mpp[s[i]]>=mpp[s[i+1]]){
+                val += mpp[s[i]];
+            }
+            else{
+                val -= mpp[s[i]];
+            }
+        }
+        val += mpp[s.back()];
+        return val;
+    }
+};

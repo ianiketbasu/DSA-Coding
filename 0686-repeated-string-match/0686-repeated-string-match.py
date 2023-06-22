ffb4ce1f0fb7db5901883ef96cb5b8f9f9baee27
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-06-22 13:38:43 +0530

    Time: 111 ms (54.54%), Space: 13.8 MB (42.42%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.py
+++ 0686-repeated-string-match/0686-repeated-string-match.py
@@ -1,15 +1,29 @@
-class Solution:
-    def repeatedStringMatch(self, a: str, b: str) -> int:
+class Solution(object):
+    def repeatedStringMatch(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        # times = -(-len(B) // len(A))  # Equal to ceil(len(B) / len(A))
+        # print(times)
+        # for i in range(2):
+        #     print((A * (times + i)))
+        #     if B in (A * (times + i)):
+        #         return times + i
+        # return -1
+    
+    
+    
         my_str = a
         count = 1
         n = len(b)//len(a)
         
         for i in range(n+2) :
-            if b in my_str :
+            if b in (my_str*(i+1)) :
                 return count
             else :
                 count += 1
-                my_str += a
+                # my_str += a
         
         return -1
-        

f42769838c043dd748892ba3e3c00adf132ba960
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-06-22 21:57:41 +0530

    Time: 46 ms (97.38%), Space: 16.9 MB (59.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(ch for ch in s)==Counter(ch for ch in t)
+        # return Counter(ch for ch in s)==Counter(ch for ch in t)
+        return Counter(s)==Counter(t)
+        
         

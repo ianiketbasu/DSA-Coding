cfa88c90eb78894bb79a3ed4d925bfd3a06d6a78
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-06-24 20:23:00 +0530

    Time: 421 ms (77.78%), Space: 14.2 MB (77.78%) - LeetHub

---
+++ 0956-tallest-billboard/0956-tallest-billboard.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def tallestBillboard(self, rods):
+        """
+        :type rods: List[int]
+        :rtype: int
+        """
+        dp = {0: 0}
+        for x in rods:
+            for d, y in dp.items():
+                dp[d + x] = max(dp.get(x + d, 0), y)
+                dp[abs(d - x)] = max(dp.get(abs(d - x), 0), y + min(d, x))
+        return dp[0]

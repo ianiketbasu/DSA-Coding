319e2a9ce0a09c2b8ff58260d5fbea55f8b15647
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-07-04 19:45:29 +0530

    Time: 83 ms (41.39%), Space: 18.2 MB (44.80%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,7 +1,18 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        # counter = Counter(nums)
+        # return next(element for element, count in counter.items() if count == 1)
         
-        counter = Counter(nums)
         
+        nums[:] = sorted(nums)
+
         
-        return next(element for element, count in counter.items() if count == 1)
+        n = len(nums)
+        if n == 1 : return nums[0]
+        if nums[0] != nums[1] : return nums[0]
+        elif nums[n-1] != nums[n-2] : return nums[n-1]
+        
+        for i in range(1,n,3) :
+            if nums[i-1] != nums[i] :
+                return nums[i-1]
+        return 0

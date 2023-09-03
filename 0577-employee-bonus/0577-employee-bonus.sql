2baac82cc200c2ff188f81cfe6786b380a6b6c3f
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-09-03 11:39:25 +0530

    Time: 2701 ms (15.24%), Space: 0B (100.00%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,5 +1,11 @@
 # Write your MySQL query statement below
-SELECT e.name , b.bonus
-from Employee as e
-LEFT JOIN Bonus as b ON e.empId = b.empId
-WHERE b.Bonus < 1000 OR b.Bonus IS NULL
+# SELECT e.name , b.bonus
+# from Employee as e
+# LEFT JOIN Bonus as b ON e.empId = b.empId
+# WHERE b.Bonus < 1000 OR b.Bonus IS NULL
+
+
+SELECT name ,bonus
+FROM Employee
+LEFT JOIN Bonus USING (empId)
+WHERE COALESCE(bonus,0) < 1000;

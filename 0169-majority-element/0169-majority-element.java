fb2e8e5ad5a10c57d9ffeb739a785cfb2af3d999
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-09-06 15:32:46 +0530

    Time: 1 ms (99.85%), Space: 48.9 MB (16.96%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,29 @@
 import java.util.Map.Entry;
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> myMap = new HashMap<>();
-        int len = nums.length;
-        for(Integer num : nums){
-            if(myMap.containsKey(num)){
-                myMap.put(num,myMap.get(num) + 1);
-            }
-            else{
-                myMap.put(num,1);
-            }
+        // HashMap<Integer,Integer> myMap = new HashMap<>();
+        // int len = nums.length;
+        // for(Integer num : nums){
+        //     if(myMap.containsKey(num)){
+        //         myMap.put(num,myMap.get(num) + 1);
+        //     }
+        //     else{
+        //         myMap.put(num,1);
+        //     }
+        // }
+        // for(Entry<Integer,Integer> entry : myMap.entrySet()){
+        //     if(entry.getValue() > len/2) return entry.getKey();
+        // }
+        // return 0;
+        
+        int count = 0;
+        int ans = 0;
+        for(int num : nums){
+            if(count == 0) ans = num;
+            if(ans == num) count++;
+            else count--;
         }
-        for(Entry<Integer,Integer> entry : myMap.entrySet()){
-            if(entry.getValue() > len/2) return entry.getKey();
-        }
-        return 0;
+        return ans;
+        
     }
 }

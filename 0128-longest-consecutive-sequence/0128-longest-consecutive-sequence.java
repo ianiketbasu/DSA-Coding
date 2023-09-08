9660886682e72f52f011d44d853a1aee7e008ad4
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-09-08 21:46:15 +0530

    Time: 25 ms (71.89%), Space: 56.2 MB (82.58%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,24 +1,37 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         
-        if(nums.length == 1) return 1;
-        if(nums.length == 0) return 0;
-        Arrays.sort(nums);
-        int count = 1;
-        int max_len = count;
+//         if(nums.length == 1) return 1;
+//         if(nums.length == 0) return 0;
+//         Arrays.sort(nums);
+//         int count = 1;
+//         int max_len = count;
         
 
-        for(int i=1;i<nums.length;i++){
-            while(i < nums.length && nums[i] == nums[i-1]) i++;
-            if(i < nums.length && nums[i] == nums[i-1] + 1){
-                count++;
-                max_len = Math.max(max_len , count);
-            }
-            else{
-                count = 1;
+//         for(int i=1;i<nums.length;i++){
+//             while(i < nums.length && nums[i] == nums[i-1]) i++;
+//             if(i < nums.length && nums[i] == nums[i-1] + 1){
+//                 count++;
+//                 max_len = Math.max(max_len , count);
+//             }
+//             else{
+//                 count = 1;
+//             }
+//         }
+        
+//         return max_len;
+        
+        
+        Set<Integer> mySet = new HashSet<>();
+        int max_len = 0;
+        for(int i=0;i<nums.length;i++) mySet.add(nums[i]);
+        for(int num : mySet){
+            if(!mySet.contains(num - 1)){
+                int y = num + 1;
+                while(mySet.contains(y)) y++;
+                max_len = Math.max(max_len,y-num);
             }
         }
-        
         return max_len;
     }
 }

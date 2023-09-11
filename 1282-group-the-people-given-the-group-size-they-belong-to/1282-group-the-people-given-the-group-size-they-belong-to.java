6efa964c26534d3e647cf6694815280a6498c437
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-09-11 14:10:17 +0530

    Time: 6 ms (83.58%), Space: 44.6 MB (46.66%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -1,26 +1,36 @@
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
         List<List<Integer>> result = new ArrayList<>();
-        HashMap<Integer,List<Integer>> map = new HashMap<>();
-        
-        for(int i=0;i<groupSizes.length;i++){
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+
+        // Step 1: Group people by their group sizes
+        for (int i = 0; i < groupSizes.length; i++) {
             int size = groupSizes[i];
-            if(map.get(size) != null){
+
+            // If the group size exists in the map, add the person's index to that group
+            if (map.containsKey(size)) {
                 map.get(size).add(i);
+            } else {
+                // If the group size doesn't exist, create a new group and add the person's index
+                map.put(size, new ArrayList<>(Arrays.asList(i)));
             }
-            else map.put(size , new ArrayList<>(Arrays.asList(i)));
         }
-        
-        for(Integer key : map.keySet()){
+
+        // Step 2: Process each group in the map
+        for (Integer key : map.keySet()) {
             List<Integer> group = map.get(key);
-            if(key == group.size())
+
+            // If the group size matches the number of people in the group, add the whole group to the result
+            if (key == group.size()) {
                 result.add(group);
-            else{
-                for(int i=0;i<group.size();i+=key){
-                    result.add(group.subList(i,i+key));
+            } else {
+                // If the group size is different, split the group into subgroups of the appropriate size
+                for (int i = 0; i < group.size(); i += key) {
+                    result.add(group.subList(i, i + key));
                 }
             }
         }
+
         return result;
     }
 }

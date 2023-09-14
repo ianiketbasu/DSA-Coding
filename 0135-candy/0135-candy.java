ad24079f52e35bed82022d23509c7173d8afda49
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-09-14 19:52:44 +0530

    Time: 283 ms (10.32%), Space: 45.1 MB (7.93%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,51 +1,39 @@
-// class Solution {
-//     public int candy(int[] ratings) {
-//         int ans = 0;
-//         int len = ratings.length;
-//         for(int i=0;i<len;++i){
-//             int prev_assign_value = 0;
-//             if(i == 0){
-//                 if(i + 1 < len){
-//                     if(ratings[i] <= ratings[i+1]){
-                        
-//                     }
-//                 }
-//                 else return 1;
-//             }else if(i == len -1){
-                
-//             }else{
-                
-//             }
-//         }
-//         return ans;
-//     }
-// }
-
 class Solution {
     public int candy(int[] ratings) {
         int len = ratings.length;
-        int[] candies = new int[len];
-        Arrays.fill(candies, 1); // Initialize candies with all 1s
+        int[] candyArr = new int[len];
         
-        // Scan left to right
-        for (int i = 1; i < len; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
+        Arrays.fill(candyArr,1);
+        
+        for(int i=1;i<len;i++){
+            if(ratings[i] > ratings[i-1]){
+                candyArr[i] = candyArr[i-1] + 1;
             }
         }
         
-        // Scan right to left
-        for (int i = len - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
-                candies[i] = candies[i + 1] + 1;
+        for(int c : candyArr){
+            System.out.print(c+",");
+        }
+        System.out.println();
+        
+        
+        for(int i=len-2;i>-1;i--){
+            if(ratings[i] > ratings[i+1] && candyArr[i] <= candyArr[i+1]){
+                candyArr[i] = candyArr[i+1] + 1;
             }
         }
         
-        int totalCandies = 0;
-        for (int candyCount : candies) {
-            totalCandies += candyCount;
+        
+        for(int c : candyArr){
+            System.out.print(c+",");
         }
         
-        return totalCandies;
+        
+        int totalCandy = 0;
+        for(int candy : candyArr){
+            totalCandy += candy;
+        }
+        
+        return totalCandy;
     }
 }

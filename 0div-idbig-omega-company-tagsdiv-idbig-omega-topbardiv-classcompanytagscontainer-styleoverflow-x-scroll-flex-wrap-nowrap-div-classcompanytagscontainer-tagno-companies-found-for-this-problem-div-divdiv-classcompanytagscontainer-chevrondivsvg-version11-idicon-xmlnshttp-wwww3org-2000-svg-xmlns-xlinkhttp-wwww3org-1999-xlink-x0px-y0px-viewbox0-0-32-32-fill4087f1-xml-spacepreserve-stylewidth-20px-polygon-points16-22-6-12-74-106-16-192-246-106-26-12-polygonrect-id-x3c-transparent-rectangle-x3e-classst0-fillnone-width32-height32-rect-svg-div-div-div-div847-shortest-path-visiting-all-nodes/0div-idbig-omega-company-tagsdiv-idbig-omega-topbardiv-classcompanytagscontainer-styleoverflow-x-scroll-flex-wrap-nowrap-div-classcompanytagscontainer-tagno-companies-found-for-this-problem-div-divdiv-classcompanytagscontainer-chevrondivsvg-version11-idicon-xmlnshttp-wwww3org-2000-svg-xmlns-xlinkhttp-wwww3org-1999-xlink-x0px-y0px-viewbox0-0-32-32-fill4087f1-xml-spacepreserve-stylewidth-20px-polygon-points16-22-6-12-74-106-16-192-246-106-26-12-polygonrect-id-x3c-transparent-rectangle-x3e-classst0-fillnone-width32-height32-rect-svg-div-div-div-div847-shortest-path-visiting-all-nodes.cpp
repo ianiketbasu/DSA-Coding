ca990bfddc2f0ecd2f8a1a831a118e699d054213
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-09-17 21:05:13 +0530

    Time: 16 ms (80.26%), Space: 13.2 MB (63.00%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div847-shortest-path-visiting-all-nodes/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div847-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+       /* int n = graph.size();
+        vector<int> v(n,INT_MAX);
+        queue<int> q;
+        
+        v[0] = 0;
+        q.push(0);
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            
+            for(auto it : graph[node]){
+                if(v[node]+1 < v[it]){
+                    v[it] = v[node] + 1;
+                    q.push(it);
+                }
+            }
+        }
+        return v[n-1];*/
+        
+        
+        int n = graph.size(),res = 0;
+    queue<tuple<int,int,int>> q;
+    vector<vector<int>> seen(n,vector<int>(1<<n));
+    for(int i=0;i<n;i++){
+        q.push(tuple<int,int,int>(i,1<<i,0));
+        seen[i][1<<i] = true;
+    }
+    while (!q.empty())
+    {
+        auto [idx,mask,dist] = q.front();
+        q.pop();
+        if(mask==(1<<n)-1){
+            res = dist;
+            break;
+        }
+        for(auto v:graph[idx]){
+            int mask_v = mask|1<<v;
+            if(!seen[v][mask_v]){
+                q.push(tuple<int,int,int>(v,mask_v,dist+1));
+                seen[v][mask_v] = true;
+            }
+        }
+    }
+    return res;
+    }
+};

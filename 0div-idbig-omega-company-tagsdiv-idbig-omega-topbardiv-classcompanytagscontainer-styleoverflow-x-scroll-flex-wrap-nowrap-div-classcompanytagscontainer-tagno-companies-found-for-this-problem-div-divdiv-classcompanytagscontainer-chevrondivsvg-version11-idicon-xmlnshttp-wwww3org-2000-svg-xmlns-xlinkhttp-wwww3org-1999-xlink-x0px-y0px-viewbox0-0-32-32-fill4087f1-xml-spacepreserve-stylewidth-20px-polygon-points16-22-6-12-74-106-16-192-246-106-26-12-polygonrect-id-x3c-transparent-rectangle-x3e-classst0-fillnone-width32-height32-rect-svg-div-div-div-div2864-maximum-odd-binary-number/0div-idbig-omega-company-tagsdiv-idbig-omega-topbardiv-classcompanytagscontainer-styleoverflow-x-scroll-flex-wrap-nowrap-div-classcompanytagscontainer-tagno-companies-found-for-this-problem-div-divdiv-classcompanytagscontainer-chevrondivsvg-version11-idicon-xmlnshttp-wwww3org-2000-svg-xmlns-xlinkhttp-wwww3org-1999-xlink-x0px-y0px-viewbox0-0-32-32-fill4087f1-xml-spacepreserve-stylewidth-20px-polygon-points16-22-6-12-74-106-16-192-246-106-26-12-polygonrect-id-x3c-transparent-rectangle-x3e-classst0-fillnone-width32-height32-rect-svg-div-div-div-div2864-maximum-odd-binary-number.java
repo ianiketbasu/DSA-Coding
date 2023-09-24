3575fc53b0682010c72ca73613146872f97232cb
Aniket Basu <81218415+ianiketbasu@users.noreply.github.com>
2023-09-24 11:19:19 +0530

    Time: 2 ms (100.00%), Space: 41.8 MB (100.00%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2864-maximum-odd-binary-number/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2864-maximum-odd-binary-number.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2864-maximum-odd-binary-number/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2864-maximum-odd-binary-number.java
@@ -1,18 +1,32 @@
-class Solution {
+public class Solution {
     public String maximumOddBinaryNumber(String s) {
-        int count_one = 0;
-        int count_zero = 0;
-        
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i) == '1') count_one++;
-            else count_zero++;
+        int n = s.length();
+        int countOne = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1') {
+                countOne++;
+            }
         }
-        
-        String ans = "";
-        for(int i=0;i<count_one - 1;i++) ans += "1";
-        for(int i=0;i<count_zero;i++) ans += "0";
-        if(count_one > 0) ans += "1";
-        
-        return ans;
+
+        int countZero = n - countOne;
+
+        // Create a StringBuilder to modify the string in place
+        StringBuilder modifiedString = new StringBuilder(s);
+
+        // Set '1's for the first countOne - 1 characters
+        for (int i = 0; i < countOne - 1; i++) {
+            modifiedString.setCharAt(i, '1');
+        }
+
+        // Set '0's for the next countZero characters
+        for (int i = countOne - 1; i < n - 1; i++) {
+            modifiedString.setCharAt(i, '0');
+        }
+
+        // Set the last character to '1'
+        modifiedString.setCharAt(n - 1, '1');
+
+        return modifiedString.toString();
     }
 }
